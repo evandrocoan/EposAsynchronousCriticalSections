4a5916c2c0b71a1b78276d5a7da170f9ba4f6488
evandrocoan <evandrocoan@hotmail.com>
2018-11-02 13:17:02 -0300

    Converted test_variadic_critical_section_forwarding.cpp to closure
    example initial test.

--- test_variadic_critical_section_forwarding.cpp
+++ test_variadic_critical_section_forwarding.cpp
@@ -1,33 +1,38 @@
 #include <stdio.h>
 
 template<typename ... Tn>
-class Critical_Section
+class Closure
 {
 public:
     typedef void (*Function)(Tn ...);
     Function _entry;
 
-    Critical_Section(Function _entry, Tn ... an): _entry(_entry)
+    Closure(Function _entry, Tn ... an): _entry(_entry)
     {
-        printf("Running Critical_Section\n");
-        this->_entry('b', 2);
+        printf( "Running Closure\n" );
+    }
+
+    void operator()() 
+    {
+        this->_entry( 'b', 2 );
     }
 };
 
-void test_function2(char arg1, int arg2)
+void test_function(char arg1, int arg2)
 {
-    printf("Running test_function2\n");
+    printf("Running test_function\n");
 }
 
 template<typename ... Tn>
-void test_function1(Tn ... an)
+Closure<Tn ...> create_closure(void(*_entry)(Tn ...), Tn ... an)
 {
-    printf("Running test_function1\n");
-    new Critical_Section<Tn ...>(&test_function2, an ...);
+    printf( "Running create_closure\n" );
+    return *( new Closure<Tn ...>( _entry, an ... ) );
 }
 
 int main()
 {
-    test_function1('a', 1);
+    auto my_closure = create_closure( &test_function, 'a', 1 );
+    my_closure();
 }
 
