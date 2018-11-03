d9eb98ef3036a9951f171e4c967749932216fd3c
evandrocoan <evandrocoan@hotmail.com>
2018-11-02 22:43:38 -0300

    Updated epos2trunk/app/producer_consumer.cc

--- epos2trunk/app/producer_consumer.cc
+++ epos2trunk/app/producer_consumer.cc
@@ -5,6 +5,7 @@
 #include <semaphore.h>
 #include <alarm.h>
 #include <guard.h>
+#include <future.h>
 
 using namespace EPOS;
 
@@ -34,31 +35,92 @@ void produce()
     in = (in + 1) % BUF_SIZE;
 } 
 
-
 int consumer()
 {
-    for(int i = 0; i < iterations; i++) {
-        buffer_guard.submit(new Critical_Section<int>(&consume, new Future<int>()));
+    for(int index = 0; index < iterations; index++) {
+        buffer_guard.submit(&consume);
         Alarm::delay(5000);
+    }
+    return 0;
+}
+
+int producer()
+{
+    for(int index = 0; index < iterations; index++) {
+        Alarm::delay(5000);
+        buffer_guard.submit(&produce);
     }
     return 0;
 }
 
 int main()
 {
-    Thread * cons = new Thread(&consumer);
+    Thread * consumer = new Thread(&consumer);
+    Thread * producer = new Thread(&producer);
 
-    // producer
-    for(int i = 0; i < iterations; i++) {
-        Alarm::delay(5000);
-        buffer_guard.submit(new Critical_Section<int>(&produce, new Future<int>()));
-    }
-
-    cons->join();
+    consumer->join();
+    producer->join();
 
     cout << "The end!" << endl;
 
-    delete cons;
+    delete consumer;
+    delete producer;
 
     return 0;
 }
+
+// // EPOS Synchronizer Component Test Program
+
+// #include <utility/ostream.h>
+// #include <thread.h>
+// #include <semaphore.h>
+// #include <alarm.h>
+
+// using namespace EPOS;
+
+// const int iterations = 100;
+
+// OStream cout;
+
+// const int BUF_SIZE = 16;
+// char buffer[BUF_SIZE];
+// Semaphore empty(BUF_SIZE);
+// Semaphore full(0);
+
+// int consumer()
+// {
+//     int out = 0;
+//     for(int i = 0; i < iterations; i++) {
+//         full.p();
+//         cout << "C<-" << buffer[out] << "\t";
+//         out = (out + 1) % BUF_SIZE;
+//         Alarm::delay(5000);
+//         empty.v();
+//     }
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     Thread * cons = new Thread(&consumer);
+
+//     // producer
+//     int in = 0;
+//     for(int i = 0; i < iterations; i++) {
+//         empty.p();
+//         Alarm::delay(5000);
+//         buffer[in] = 'a' + in;
+//         cout << "P->" << buffer[in] << "\t";
+//         in = (in + 1) % BUF_SIZE;
+//         full.v();
+//     }
+
+//     cons->join();
+
+//     cout << "The end!" << endl;
+
+//     delete cons;
+
+//     return 0;
+// }
