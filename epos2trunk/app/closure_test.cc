1dcf9729d076b607dcb331a15394286ad9141aca
evandrocoan <evandrocoan@hotmail.com>
2018-11-03 10:50:24 -0300

    Created the epos2trunk/app/closure_test.cc example

---
+++ epos2trunk/app/closure_test.cc
@@ -0,0 +1,29 @@
+// EPOS Synchronizer Component Test Program
+
+#include <thread.h>
+#include <machine.h>
+#include <utility/guard.h>
+#include <alarm.h>
+
+using namespace EPOS;
+
+// #include <semaphore.h>
+// Semaphore display_lock;
+// #define log(argument) display_lock.p(); db<Synchronizer>(WRN) << argument; display_lock.v();
+#define log(argument) db<Synchronizer>(WRN) << argument;
+
+Guard counter_guard;
+
+int show(char arg1, const char * arg2, bool arg3, bool arg4, int arg5) {
+    log( "arg1=" << arg1 << ", arg2=" << arg2 << ", arg3=" << arg3 << ", arg4=" << arg4 << ", arg5=" << arg5 << endl )
+    return 0;
+}
+
+int main()
+{
+    log( "main: begin()" << endl )
+    counter_guard.submit(&show, 'A', "Test 1", true, false, 10);
+
+    log( "main: exiting()" << endl )
+    return 0;
+}
