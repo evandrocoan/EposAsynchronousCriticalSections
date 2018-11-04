7e5774c2cc4f9d157b5c68e604d04cc8708721dc
evandrocoan <evandrocoan@hotmail.com>
2018-11-04 16:28:07 -0200

    Created the parameter_pack_sequencer_size_generator.cpp example

---
+++ parameter_pack_sequencer_size_expanded.cpp
@@ -0,0 +1,31 @@
+template <int ...> struct MetaSequenceOfIntegers {
+};
+template<> struct MetaSequenceOfIntegers<<5, 1, 0>> {
+};
+template <int AccumulatedSize, typename Tn, int ...GeneratedSequence> struct GeneratorOfIntegerSequence
+template<> struct GeneratorOfIntegerSequence<0, int (char, int, char), <>> {
+    typedef typename GeneratorOfIntegerSequence<0 + sizeof(char), int (int, char), 0>::type type;
+}
+template<> struct GeneratorOfIntegerSequence<1, int (int, char), <0>> {
+    typedef typename GeneratorOfIntegerSequence<1 + sizeof(int), int (char), 1, 0>::type type;
+}
+template<> struct GeneratorOfIntegerSequence<5, int (char), <1, 0>> {
+    typedef typename GeneratorOfIntegerSequence<5 + sizeof(char), int (), 5, 1, 0>::type type;
+}
+template<> struct GeneratorOfIntegerSequence<6, int (), <5, 1, 0>> {
+    typedef MetaSequenceOfIntegers<5, 1, 0> type;
+};
+template <int AccumulatedSize, typename Grouper, typename Head, typename ...Tail, int ...GeneratedSequence> struct GeneratorOfIntegerSequence<AccumulatedSize, type-parameter-0-1 (type-parameter-0-2, type-parameter-0-3...), <GeneratedSequence...>> {
+    typedef typename GeneratorOfIntegerSequence<AccumulatedSize + sizeof(Head), Grouper (Tail...), AccumulatedSize, GeneratedSequence...>::type type;
+};
+template <int AccumulatedSize, typename Grouper, int ...GeneratedSequence> struct GeneratorOfIntegerSequence<AccumulatedSize, type-parameter-0-1 (), <GeneratedSequence...>> {
+    typedef MetaSequenceOfIntegers<GeneratedSequence...> type;
+};
+template <int ...Sequence> void _run(MetaSequenceOfIntegers<Sequence...>) {
+}
+template<> void _run<<5, 1, 0>>(MetaSequenceOfIntegers<5, 1, 0>) {
+}
+int main(int argc, const char *argv[]) {
+    _run(GeneratorOfIntegerSequence<0, int (char, int, char)>::type());
+    return 0;
+}
