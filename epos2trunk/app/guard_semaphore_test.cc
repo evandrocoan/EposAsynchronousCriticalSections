c8a02e41ff1217476294a4ff9266dea5764555cc
evandrocoan <evandrocoan@hotmail.com>
2018-11-13 13:55:02 -0200

    Replaced the terminal positional messages by sequential prints

--- epos2trunk/app/guard_semaphore_test.cc
+++ epos2trunk/app/guard_semaphore_test.cc
@@ -14,28 +14,32 @@ const int iterations = 2;
 #define log(argument) db<Synchronizer>(WRN) << argument;
 
 Guard table;
+Guard guard;
 OStream cout;
 
 Thread * phil[5];
 Semaphore * chopstick[5];
+// bool chopstick[5];
 
 void show_message(const char * message, int line, int column) {
-    Display::position( line, column );
-    cout << message;
+    // Display::position( line, column );
+    cout << message << "(" << line << ")" << endl;
 }
 
 void show_message(StringStream * message, int line, int column) {
-    Display::position( line, column );
-
+    // Display::position( line, column );
     cout << message;
     delete message;
 }
+
+// void get_chopstick(Future<int>* ) {
+// }
 
 int philosopher(int philosopher_index, int line, int column)
 {
     int first = (philosopher_index < 4)? philosopher_index : 0;
     int second = (philosopher_index < 4)? philosopher_index + 1 : 4;
-
+    line = philosopher_index;
     for(int i = iterations; i > 0; i--)
     {
         table.submit( &show_message, "thinking", line, column );
@@ -44,11 +48,27 @@ int philosopher(int philosopher_index, int line, int column)
         chopstick[first]->p();    // get first chopstick
         chopstick[second]->p();   // get second chopstick
 
+        StringStream* stream1 = new StringStream{100};
+        *stream1 << "Philosopher=" << philosopher_index << " got      the first=" << first 
+                << " and second=" << second << " chopstick\n";
+        table.submit( &show_message, stream1, line, column );
+
+        // Future<bool>* first_chopstick = new Future<bool>();
+        // Future<bool>* second_chopstick = new Future<bool>();
+        // guard.submit( &get_chopstick, first_chopstick, second_chopstick )
+
         table.submit( &show_message, " eating ", line, column );
         Delay eating(1000000);
 
         chopstick[first]->v();    // release first chopstick
         chopstick[second]->v();   // release second chopstick
+
+        StringStream* stream2 = new StringStream{100};
+        *stream2 << "Philosopher=" << philosopher_index << " released the first=" << first 
+                << " and second=" << second << " chopstick\n";
+        table.submit( &show_message, stream2, line, column );
+        
+        // guard.submit( &release_chopstick, first_chopstick, second_chopstick )
     }
 
     table.submit( &show_message, "  done  ", line, column );
@@ -62,6 +82,7 @@ void setup_program()
     cout << "The Philosopher's Dinner:" << endl;
 
     for(int i = 0; i < 5; i++)
+        // chopstick[i] = true;
         chopstick[i] = new Semaphore;
 
     phil[0] = new Thread(&philosopher, 0,  5, 32);
@@ -99,8 +120,6 @@ int main()
         table.submit( &show_message, stream, 20 + i, 0 );
     }
 
-    for(int i = 0; i < 5; i++)
-        delete chopstick[i];
     for(int i = 0; i < 5; i++)
         delete phil[i];
 
