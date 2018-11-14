d120577d48bd8e660ca4c5c998e0ee8255bb32bd
evandrocoan <evandrocoan@hotmail.com>
2018-11-14 13:25:50 -0200

    Deprecated closure_guard_test.cc log() db<Synchronizer>(WRN) by
    debug_sync.h header.

--- epos2trunk/app/closure_guard_test.cc
+++ epos2trunk/app/closure_guard_test.cc
@@ -1,28 +1,28 @@
 // EPOS Synchronizer Component Test Program
+#define DEBUG_SYNC
 
 #include <thread.h>
 #include <machine.h>
 #include <utility/guard.h>
+#include <utility/debug_sync.h>
 #include <alarm.h>
 
 using namespace EPOS;
-#define log(argument) db<Synchronizer>(WRN) << argument;
-
 Guard counter_guard;
 
-void show(char char1, const char* text1, const char* text2, 
+void show(char char1, const char* text1, const char* text2,
             bool bool1, bool bool2, const char* text3, int int1) {
-    log( "    char1=" << char1 << ", text1=" << text1 \
-        << ", text2=" << text2 << ", bool1=" << bool1 << ", bool2=" << bool2 \
-        << ", text3=" << text3 << ", int1="  << int1  << endl << endl )
+    LOG( Debug, WRN, "    char1=" << char1 << ", text1=" << text1
+        << ", text2=" << text2 << ", bool1=" << bool1 << ", bool2=" << bool2
+        << ", text3=" << text3 << ", int1="  << int1  << endl )
 }
 
 int main()
 {
-    log( endl << "main: begin()" << endl )
+    LOG( Debug, WRN, endl << "main: begin()" << endl )
     counter_guard.submit(&show, 'A', "Test 1", "Test 2", true, false, "Test 3", 10);
     counter_guard.submit(&show, 'B', "Test 4", "Test 5", false, false, "Test 6", 20);
 
-    log( "main: exiting()" << endl )
+    LOG( Debug, WRN, "main: exiting()" << endl )
     return 0;
 }
