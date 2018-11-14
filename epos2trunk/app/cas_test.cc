a06aec1077e443496d1ab0b63636128b8c8aa9b7
evandrocoan <evandrocoan@hotmail.com>
2018-11-14 13:55:19 -0200

    Deprecated cas_test.cc log() db<Synchronizer>(WRN) by
    debug_sync.h header.

--- epos2trunk/app/cas_test.cc
+++ epos2trunk/app/cas_test.cc
@@ -1,32 +1,35 @@
 // EPOS CAS Component Test Program
+#define DEBUG_SYNC
 
 #include <utility/ostream.h>
 #include <architecture/ia32/cpu.h>
+#include <utility/debug_sync.h>
 
 using namespace EPOS;
 OStream cout;
 
 int main()
 {
-    cout << endl << "Welcome to the CPU::cas() instruction test!" << endl;
+    LOG( Debug, WRN, endl )
+    LOG( Debug, WRN, "Welcome to the CPU::cas() instruction test!" << endl )
     int original = 5;
     int compare = 5;
     int replacement = 6;
     int replaced;
 
-    cout << "original=" << original
+    LOG( Debug, WRN, "original=" << original
             << ", compare=" << compare
             << ", replacement=" << replacement
             << ", replaced=" << replaced
-            << endl;
+            << endl )
 
     replaced = CPU::cas(original, compare, replacement);
 
-    cout << "original=" << original
+    LOG( Debug, WRN, "original=" << original
             << ", compare=" << compare
             << ", replacement=" << replacement
             << ", replaced=" << replaced
-            << endl;
+            << endl )
 
-    cout << "The CPU::cas() instruction set ran successfully!" << endl << endl;
+    LOG( Debug, WRN, "The CPU::cas() instruction set ran successfully!" << endl )
 }
