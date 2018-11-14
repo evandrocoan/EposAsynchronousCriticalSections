e03f0de9eb2b139372a908223b493b461d8db58a
evandrocoan <evandrocoan@hotmail.com>
2018-11-14 13:35:56 -0200

    Deprecated closure_test.cc log() db<Synchronizer>(WRN) by
    debug_sync.h header.

--- epos2trunk/app/closure_test.cc
+++ epos2trunk/app/closure_test.cc
@@ -1,15 +1,14 @@
 // EPOS Synchronizer Component Test Program
+#define DEBUG_SYNC
 
 #include <utility/closure.h>
-
 using namespace EPOS;
-#define log(argument) db<Synchronizer>(WRN) << argument;
 
 template<typename ReturnType, typename ... Tn>
 Closure< ReturnType(Tn ...) > create_closure( ReturnType(*_entry)( Tn ... ), Tn ... an )
 {
     auto closure = new Closure< ReturnType(Tn ...) >( _entry, an ... );
-    log( "create_closure " << closure << endl )
+    LOG( Debug, WRN, "create_closure " << closure << endl )
     return *closure;
 }
 
@@ -17,41 +16,42 @@ template<typename ... Tn>
 void test_closure(Tn ... an) {
     auto closure = create_closure(an ...);
     closure();
-    log( "    void" << endl << endl )
+    LOG( Debug, WRN, "    void" << endl << endl )
 }
 
 template<typename ... Tn>
 void test_closure_with_return(Tn ... an) {
     auto closure = create_closure(an ...);
-    log( "    " << closure() << endl << endl )
+    auto return_value = closure();
+    LOG( Debug, WRN, "    " << return_value << endl << endl )
 }
 
 char test_function1(char arg1, int arg2, bool arg3) {
-    log( "test_function1 " << arg1 << ", " <<  arg2 << ", " << arg3 << endl )
+    LOG( Debug, WRN, "test_function1 " << arg1 << ", " <<  arg2 << ", " << arg3 << endl )
     return 'A';
 }
 
 char test_function2(const char* arg1, const char* arg2, char arg3) {
-    log( "test_function2 " << arg1 << ", " <<  arg2 << ", " << arg3 << endl )
+    LOG( Debug, WRN, "test_function2 " << arg1 << ", " <<  arg2 << ", " << arg3 << endl )
     return 'B';
 }
 
 char test_function3() {
-    log( "test_function3 " << endl )
+    LOG( Debug, WRN, "test_function3 " << endl )
     return 'C';
 }
 
 void test_function4() {
-    log( "test_function4 " << endl )
+    LOG( Debug, WRN, "test_function4 " << endl )
 }
 
 void test_function5(const char* arg1) {
-    log( "test_function5 " << arg1 << endl )
+    LOG( Debug, WRN, "test_function5 " << arg1 << endl )
 }
 
 int main()
 {
-    log( endl << "main: begin()" << endl )
+    LOG( Debug, WRN, endl << "main: begin()" << endl )
 
     test_closure_with_return( &test_function1, 'a', 10, false );
     test_closure_with_return( &test_function2, "test1", "test2", 'b' );
@@ -61,6 +61,6 @@ int main()
     test_closure( &test_function5, "Testa 3" );
     test_closure( &test_function5, "Testa 4" );
 
-    log( "main: exiting()" << endl )
+    LOG( Debug, WRN, "main: exiting()" << endl )
     return 0;
 }
