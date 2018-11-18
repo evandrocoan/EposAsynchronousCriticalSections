628b85ae4c41d37d1d34cbccd0a3d35bcf8da562
evandrocoan <evandrocoan@hotmail.com>
2018-11-17 22:35:25 -0200

    Included guard.h directly on guard_scheduler_cpu_affinity_test.cpp

--- non-epos-guard/count_sync_guard/guard.h
+++ non-epos-guard/count_sync_guard/guard.h
@@ -267,16 +267,3 @@ public:
         return next;
     }
 };
-
-void example() {
-    DB( "Example begin!" << std::endl )
-}
-
-// g++ -o test $APPLICATION -lpthread -m32 && ./test
-// sudo chrt --rr 99 ./guarded_count &> test_guarded.log
-int main() {
-    DB( "Main begin!" << std::endl )
-    Guard guard;
-
-    guard.submit(example);
-}
