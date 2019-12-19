52c64e34aca3b53099b439a6a531415ff7055937
evandrocoan <evandrocoan@hotmail.com>
2019-12-18 22:50:49 -0300

    Added reference to non-epos-guard/string_encrytion.cpp

--- non-epos-guard/string_encrytion.cpp
+++ non-epos-guard/string_encrytion.cpp
@@ -11,6 +11,7 @@ char const EncryptCharsA<Chars...>::value[sizeof...(Chars) + 1] = {
     add_three<Chars>::value...
 };
 
+// https://stackoverflow.com/questions/3492742/compile-time-string-manipulation-with-variadic-templates
 // /usr/bin/clang++ -Xclang -ast-print -fsyntax-only test_debugger.cpp -DTINYFORMAT_FORMATTER_DEBUGGER_LEVEL=127 > main.exe
 int main() {
     EncryptCharsA<'A','B','C'>::value;
